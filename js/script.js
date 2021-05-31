c09f82e95e51013e99672f0ef0a824ad9794afc3
mynombre <david_dc381@hotmail.com>
2021-05-31 19:21:16 -0400

    Se agregó el cambio de color al hacer scroll en el menu de navegación

---
+++ js/script.js
@@ -0,0 +1,23 @@
+(function(){
+
+  let bar = document.getElementById('bar');
+  let heightHeader = window.scrollY;
+
+  if (window.innerWidth >= 769) {
+    // bar.style.backgroundColor = "transparent";
+
+    window.onscroll = () => {
+      if (document.documentElement.scrollTop >= 400) {
+        bar.style.backgroundColor = "#ffc107";
+        bar.style.transition = "all .5s ease-out";
+      } else {
+        bar.style.backgroundColor = "transparent";
+        bar.style.transition = "all .5s ease-in";
+      }
+    };
+  } else if (window.innerWidth <= 768) {
+    bar.style.backgroundColor = "#ffc107";
+  }
+ 
+
+})();
